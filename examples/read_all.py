b9dd31503a031b5e5f78502bc2c896f761b0abea
Cavon Lee <cavonxx@gmail.com>
2024-04-19 10:04:41 +0800

    Add examples, cleanup code

---
+++ examples/read_all.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+from spc.spc import SPC
+import time
+
+spc = SPC()
+
+def main():
+    print(f"Firmware Version: {spc.read_firmware_version()}")
+    time.sleep(2)
+
+    while True:
+        # Read the data before clearing the screen，to retain the last data when an error occurs.
+        data_buffer = spc.read_all()
+
+        print(f"Input voltage: {data_buffer['input_voltage']} mV")
+        print(f"Raspberry Pi voltage: {data_buffer['output_voltage']} mV")
+        print(f"Battery voltage: {data_buffer['battery_voltage']} mV")
+        print(f"Battery percentage: {data_buffer['battery_percentage']} %")
+        print(f"Power source: {data_buffer['power_source']} - {'Battery' if data_buffer['power_source'] == spc.BATTERY else 'External'}")
+        print(f"Input plugged in: {data_buffer['is_input_plugged_in']}")
+        print(f"Battery plugged in: {data_buffer['is_battery_plugged_in']}")
+        print(f"Charging: {data_buffer['is_charging']}")
+        print('')
+        print(f"Internal data:")
+        shutdown_request_str = 'None'
+        if data_buffer['shutdown_request'] == spc.SHUTDOWN_REQUEST_NONE:
+            shutdown_request_str = 'None'
+        elif data_buffer['shutdown_request'] == spc.SHUTDOWN_REQUEST_LOW_BATTERY:
+            shutdown_request_str = 'Low battery'
+        elif data_buffer['shutdown_request'] == spc.SHUTDOWN_REQUEST_BUTTON:
+            shutdown_request_str = 'Button'
+        else:
+            shutdown_request_str = 'Unknown'
+        print(f"Shutdown request: {data_buffer['shutdown_request']} - {shutdown_request_str}")
+        print(f'Board id: {spc.read_board_id()}')
+        print(f"Always on: {spc.read_always_on()}")
+        print(f"Power source voltage: {spc.read_power_source_voltage()} mV")
+        print(f"Shutdown percentage: {spc.read_shutdown_percentage()} %")
+        print(f"Power off percentage: {spc.read_power_off_percentage()} %")
+
+        print('')
+        print('')
+        time.sleep(1)
+
+if __name__ == '__main__':
+    main()
--- pipower3/__init__.py
+++ pipower3/__init__.py
@@ -7,25 +7,24 @@ def main():
     spc = SPC()
     parser = argparse.ArgumentParser(description='PiPower 3')
     parser.add_argument('command', nargs='?', help='Command')
-    parser.add_argument('-sb', '--shutdown_battery_percentage', nargs='?', default='', help='Set shutdown battery percentage, leave empty to read')
-    parser.add_argument('-so', '--shutdown_override', help='Set shutdown override, leave empty to read')
-    parser.add_argument('-iv', '--input_voltage', action='store_true', help='Read input voltage')
-    parser.add_argument('-ov', '--output_voltage', action='store_true', help='Read output voltage')
-    parser.add_argument('-bv', '--battery_voltage', action='store_true', help='Read battery voltage')
-    parser.add_argument('-bp', '--battery_percentage', action='store_true', help='Read battery percentage')
-    parser.add_argument('-bs', '--battery_source', action='store_true', help='Read battery source')
-    parser.add_argument('-ii', '--is_input_plugged_in', action='store_true', help='Read is input plugged in')
-    parser.add_argument('-ib', '--is_battery_plugged_in', action='store_true', help='Read is battery plugged in')
-    parser.add_argument('-ic', '--is_charging', action='store_true', help='Read is charging')
-    parser.add_argument('-ao', '--always_on', action='store_true', help='Read always on')
-    parser.add_argument('-sr', '--shutdown_request', action='store_true', help='Read shutdown request')
-    parser.add_argument('-bi', '--board_id', action='store_true', help='Read board id')
-    parser.add_argument('-psv', '--power_source_voltage', action='store_true', help='Read power source voltage')
+    parser.add_argument('-sp', '--shutdown-percentage', nargs='?', default='', help='Set shutdown percentage, leave empty to read')
+    parser.add_argument('-pp', '--power-off-percentage', nargs='?', default='', help='Set power off percentage, leave empty to read')
+    parser.add_argument('-iv', '--input-voltage', action='store_true', help='Read input voltage')
+    parser.add_argument('-ov', '--output-voltage', action='store_true', help='Read output voltage')
+    parser.add_argument('-bv', '--battery-voltage', action='store_true', help='Read battery voltage')
+    parser.add_argument('-bp', '--battery-percentage', action='store_true', help='Read battery percentage')
+    parser.add_argument('-bs', '--battery-source', action='store_true', help='Read battery source')
+    parser.add_argument('-ii', '--is-input-plugged_in', action='store_true', help='Read is input plugged in')
+    parser.add_argument('-ib', '--is-battery-plugged_in', action='store_true', help='Read is battery plugged in')
+    parser.add_argument('-ic', '--is-charging', action='store_true', help='Read is charging')
+    parser.add_argument('-ao', '--always-on', action='store_true', help='Read always on')
+    parser.add_argument('-sr', '--shutdown-request', action='store_true', help='Read shutdown request')
+    parser.add_argument('-bi', '--board-id', action='store_true', help='Read board id')
+    parser.add_argument('-psv', '--power_source-voltage', action='store_true', help='Read power source voltage')
     parser.add_argument('-a', '--all', action='store_true', help='All')
 
     args = parser.parse_args()
 
-    print (args)
     if args.command == 'start':
         import os
         from pipower3.logger import Logger
@@ -71,19 +70,32 @@ def main():
         os.system('kill -9 $(pgrep -f "pipower3 start")')
         os.system('kill -9 $(pgrep -f "pipower3-service start")')
 
-    if args.shutdown_battery_percentage != '':
-        if args.shutdown_battery_percentage == None:
-            print(f"Shutdown battery percentage: {spc.read_shutdown_battery_percentage()}%")
+    if args.shutdown_percentage != '':
+        if args.shutdown_percentage == None:
+            print(f"Shutdown battery percentage: {spc.read_shutdown_percentage()}%")
         else:
-            if int(args.shutdown_battery_percentage) < 10:
+            if int(args.shutdown_percentage) < 10:
                 print("Failed, shutdown battery percentage minimal is 10%")
-            elif int(args.shutdown_battery_percentage) > 100:
+            elif int(args.shutdown_percentage) > 100:
                 print("Failed, shutdown battery percentage maximal is 100%")
             else:
-                spc.write_shutdown_battery_percentage(int(args.shutdown_battery_percentage))
+                spc.write_shutdown_percentage(int(args.shutdown_percentage))
                 time.sleep(0.5)
-                if spc.read_shutdown_battery_percentage() == int(args.shutdown_battery_percentage):
-                    print(f"Success, shutdown battery percentage: {spc.read_shutdown_battery_percentage()}%")
+                if spc.read_shutdown_percentage() == int(args.shutdown_percentage):
+                    print(f"Success, shutdown battery percentage: {spc.read_shutdown_percentage()}%")
+    if args.power_off_percentage != '':
+        if args.power_off_percentage == None:
+            print(f"Power off battery percentage: {spc.read_power_off_percentage()}%")
+        else:
+            if int(args.power_off_percentage) < 5:
+                print("Failed, power off battery percentage minimal is 5%")
+            elif int(args.power_off_percentage) > 100:
+                print("Failed, power off battery percentage maximal is 100%")
+            else:
+                spc.write_power_off_percentage(int(args.power_off_percentage))
+                time.sleep(0.5)
+                if spc.read_power_off_percentage() == int(args.power_off_percentage):
+                    print(f"Success, power off battery percentage: {spc.read_power_off_percentage()}%")
     if args.input_voltage:
         print(f"Input voltage: {spc.read_input_voltage()} mV")
     if args.output_voltage:
@@ -104,7 +116,17 @@ def main():
     if args.always_on:
         print(f"Always on: {spc.read_always_on()}")
     if args.shutdown_request:
-        print(f"Shutdown request: {spc.read_shutdown_request()}")
+        shutdown_request = spc.read_shutdown_request()
+        shutdown_request_str = 'None'
+        if shutdown_request == spc.SHUTDOWN_REQUEST_NONE:
+            shutdown_request_str = 'None'
+        elif shutdown_request == spc.SHUTDOWN_REQUEST_LOW_BATTERY:
+            shutdown_request_str = 'Low battery'
+        elif shutdown_request == spc.SHUTDOWN_REQUEST_BUTTON:
+            shutdown_request_str = 'Button'
+        else:
+            shutdown_request_str = 'Unknown'
+        print(f"Shutdown request: {shutdown_request} - {shutdown_request_str}")
     if args.board_id:
         print(f"Board id: {spc.read_board_id()}")
     if args.power_source_voltage:
@@ -125,8 +147,18 @@ def main():
             print(f"Battery plugged in: {data_buffer['is_battery_plugged_in']}")
             print(f"Charging: {data_buffer['is_charging']}")
             print(f"Fan power: {data_buffer['fan_power']} %")
-            print(f"Shutdown battery percentage: {data_buffer['shutdown_battery_percentage']} %")
-            print(f"Shutdown request: {data_buffer['shutdown_request']}")
+            shutdown_request_str = 'None'
+            if data_buffer['shutdown_request'] == spc.SHUTDOWN_REQUEST_NONE:
+                shutdown_request_str = 'None'
+            elif data_buffer['shutdown_request'] == spc.SHUTDOWN_REQUEST_LOW_BATTERY:
+                shutdown_request_str = 'Low battery'
+            elif data_buffer['shutdown_request'] == spc.SHUTDOWN_REQUEST_BUTTON:
+                shutdown_request_str = 'Button'
+            else:
+                shutdown_request_str = 'Unknown'
+            print(f"Shutdown request: {data_buffer['shutdown_request']} - {shutdown_request_str}")
             print(f'Board id: {spc.read_board_id()}')
             print(f"Always on: {spc.read_always_on()}")
             print(f"Power source voltage: {spc.read_power_source_voltage()} mV")
+            print(f"Shutdown percentage: {spc.read_shutdown_percentage()} %")
+            print(f"Power off percentage: {spc.read_power_off_percentage()} %")
